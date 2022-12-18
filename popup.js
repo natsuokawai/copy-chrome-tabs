a741f2cddd712cba23c6d24b6da9b3e5974e4902
natsuokawai <kawai.natsuo@gmail.com>
2022-12-18 21:17:54 +0900

    Copy title and url of current tabs

---
+++ popup.js
@@ -0,0 +1,16 @@
+const main = document.getElementById('main');
+
+const currentWindow = await chrome.windows.getCurrent();
+const tabs = await chrome.tabs.query({windowId: currentWindow.id, url: '*://*/*'});
+
+const extractTabTitleAndUrl = (tabs) => tabs.map((tab) => `[${tab.title}](${tab.url})`).join('\n');
+
+const button = main.querySelector('button');
+button.addEventListener('click', async _event => {
+  try {
+    await navigator.clipboard.writeText(extractTabTitleAndUrl(tabs));
+  } catch (err) {
+    console.log(err);
+  }
+  window.close();
+});
